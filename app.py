7b4998b8a667a02568cdf1f0c62e271b42e508a6
Felipe Marques <felipe.comarques1208@gmail.com>
2023-09-07 01:37:53 -0300

    doc: created Swagger documentation

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from model.schemas import ma
 from controller.categoryResource import CategoryResource
 from controller.manufacturerResource import ManufacturerResource
 from controller.productResource import ProductResource
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -15,6 +16,17 @@ ma.init_app(app)
 api.add_resource(CategoryResource, '/categories', '/categories/<int:category_id>')
 api.add_resource(ManufacturerResource, '/manufacturers', '/manufacturers/<int:manufacturer_id>')
 api.add_resource(ProductResource, '/products', '/products/<int:product_id>')
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
+
+swaggerui_blueprint = get_swaggerui_blueprint( SWAGGER_URL, API_URL,
+    config={  
+        'app_name': "supermarket-api"
+    },
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 with app.app_context():
   db.create_all()
